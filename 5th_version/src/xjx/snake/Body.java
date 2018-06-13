6ee07e6f0e04d073a835f16bce07e9e74cc03abf
Jiaxin Tse <thexjx@gmail.com>
2018-06-13 18:28:36 +0800

    Create Body.java

---
+++ 5th_version/src/xjx/snake/Body.java
@@ -0,0 +1,27 @@
+package xjx.snake;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import xjx.game.*;
+
+//身体节点的数据结构
+public class Body {
+	Coordinate coor;//坐标
+	JLabel label;
+	public Body(int x,int y,ImageIcon icon)
+	{
+		coor = new Coordinate(x,y);
+		label = new JLabel(icon);
+	}
+	
+	public Body(Coordinate coor,ImageIcon icon){
+		this.coor = coor;
+		label = new JLabel(icon);
+	}
+	
+	public Body(Body temp){
+		coor = temp.coor;
+		label = temp.label;
+	}
+}
